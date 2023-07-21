247e31eb9cd4d7f8108f64a56f7e3f732a96a340
skdhameliya <smitdhameliya10@gmail.com>
2023-07-21 15:53:43 -0400

    added cyclic s

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,4 +1,6 @@
 export let CONSTANTS = {
-    server_url : "https://nice-teal-nematode-slip.cyclic.app",
-    client_url : ""
+    server_url : "https://teal-repulsive-turtle.cyclic.app",
+    // server_url : "https://rider-api-ckvg.onrender.com",
+    // server_url : "http://localhost:5000",
+    // client_url : ""
 }
